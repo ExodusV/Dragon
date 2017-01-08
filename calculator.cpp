a6da591816f18b58998a588c30efc921c60723f4
ExodusV <ExodusV@users.noreply.github.com>
2017-01-08 13:19:24 -0600

    Add files via upload

--- calculator.cpp
+++ calculator.cpp
@@ -9,25 +9,27 @@ int main()
     cout << "Enter operator either + or - or * or /: ";
     cin >> op;
 
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
+    cout << "Enter first operand: ";
+    cin >> num1;
+    cout << "Enter second operand: "; 
+    cin>> num2;
 
     switch(op)
     {
         case '+':
-            cout << num1+num2;
+            cout << num1+num2 << "\n";
             break;
 
         case '-':
-            cout << num1-num2;
+            cout << num1-num2<<"\n";
             break;
 
         case '*':
-            cout << num1*num2;
+            cout << num1*num2<< "\n";
             break;
 
         case '/':
-            cout << num1/num2;
+            cout << num1/num2 << "\n";
             break;
 
         default:
